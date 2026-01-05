e1566852ae0c1256f57a3e0ab33309aeed7e4440
kremzil <kremzil@gmail.com>
2026-01-05 14:58:53 +0100

    add content

---
+++ src/data/nav.ts
@@ -0,0 +1,11 @@
+export type NavItem = {
+  label: string;
+  href: string;
+};
+
+export const navItems: NavItem[] = [
+  { label: "Domov", href: "/" },
+  { label: "Kalkulačka zloženého úročenia", href: "/kalkulacka" },
+  { label: "O nás", href: "/about" },
+  { label: "Blog", href: "/blog" }
+];
